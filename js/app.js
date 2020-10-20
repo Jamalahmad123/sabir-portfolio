a2deafeb5928813fe03bcae72da77905f3ea03ff
Jamalahmad123 <45999703+Jamalahmad123@users.noreply.github.com>
2020-10-20 15:39:13 +0500

    initial commit

---
+++ js/app.js
@@ -0,0 +1,35 @@
+
+
+
+// Start Work Section PopUp Image
+
+// Get Element
+const modal = document.querySelector('.modal');
+const imagePre = document.querySelectorAll('.work-grid-item img');
+const fullImg = document.querySelector('.full-img');
+const imgContent = document.querySelector('.modal-content');
+
+// Listen for click
+
+// Open Modal
+imagePre.forEach((item) => {
+  item.addEventListener('click', function() {
+    modal.classList.add('open');
+    fullImg.classList.add('open');
+
+    // Dynamic Image and Text
+    const fullImageSrc = item.getAttribute('data-original');
+    fullImg.src = `./images/Album Cover/${fullImageSrc}`;
+    const imageText = item.alt;
+    imgContent.textContent = imageText;
+  });
+});
+// Close Modal
+modal.addEventListener('click', function(e) {
+  if(e.target.classList.contains('modal')) {
+    modal.classList.remove('open');
+    fullImg.classList.remove('open');
+  }
+})
+
+// End Work Section PopUp Image
