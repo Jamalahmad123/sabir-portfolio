2127177f6434aa8649b75e28f3d9e04cebce2937
Jamalahmad123 <45999703+Jamalahmad123@users.noreply.github.com>
2020-10-23 11:24:45 +0500

    changes

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 // Navbar UI
 const navbar = document.querySelector('.navbar');
+const navbarLinks = document.querySelector('.nav-links');
 const sidebar = document.querySelector('.sidebar');
 const hamburger = document.querySelector('.hamburger');
 const closeNav = document.querySelector('.close-btn');
@@ -39,11 +40,18 @@ const scrollLinks = document.querySelectorAll('.scroll-links');
 // Loop over each of it
 scrollLinks.forEach((linsk) => {
   linsk.addEventListener('click', function(event) {
+    // calculate the nav height
+    const navbarHeight = navbar.getBoundingClientRect().height;
+    const navLinksHeight = navbar.getBoundingClientRect().height;
+    const fixedNav = navbar.classList.contains('fixed-navbar');
     // Scroll each link
     const id = event.currentTarget.getAttribute('href').slice(1);
     const element = document.getElementById(id);
-    const position = element.offsetTop;
-    console.log(position);
+    const position = element.offsetTop - navbarHeight;
+    window.scrollTo({
+      left: 0,
+      top: position,
+    });
     // Prevent Default
     event.preventDefault();
   })
